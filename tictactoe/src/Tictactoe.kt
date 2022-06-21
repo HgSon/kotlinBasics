7b35cb735d2c60d4df5bacfebdb977ff999f3de5
손한결 <hgson.8@gmail.com>
2022-06-21 16:20:19 +0900

    TicTacToe 2/5 refactoring

--- tictactoe/src/Tictactoe.kt
+++ tictactoe/src/Tictactoe.kt
@@ -1,6 +1,6 @@
 package tictactoe
 
-class Tictactoe(val columns: Int = 3, rows: Int = 3) {
+class Tictactoe(val columns: Int = 3, val rows: Int = 3) {
     private val rowLiner = "-"
     private val columnLiner = "|"
 
@@ -8,6 +8,18 @@ class Tictactoe(val columns: Int = 3, rows: Int = 3) {
         print("Enter cells: ")
         // val input = readln()
         val input = readLine()!!
+
+        try {
+            drawGrid(input)
+        } catch (e: Exception) {
+            println(e.message)
+        }
+    }
+
+    fun drawGrid(input: String) {
+        if (input.length != columns * rows) {
+            throw Exception("[INVALID INPUT] Expected input length = ${columns * rows} , input length = ${input.length}")
+        }
 
         drawRowBoundary()
 
@@ -25,4 +37,6 @@ class Tictactoe(val columns: Int = 3, rows: Int = 3) {
         }
         println()
     }
+
+
 }
